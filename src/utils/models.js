a19795d6bf7383e93a6e013d61e2b4d5bbea6ce9
Daniel Hatas <samson84@gmail.com>
2019-05-23 23:53:26 +0200

    remove not used models

--- src/utils/models.js
+++ src/utils/models.js
@@ -1,15 +1,1 @@
-import cloneDeep from 'lodash/cloneDeep';
-
 export const breed = ({name = '', id = null} = {}) => ({name, id})
-
-export const question = (
-  {breeds = [], correctAnswer = null, images = {}} = {}
-) => ({
-    breeds: cloneDeep(breeds),
-    correctAnswer,
-    images: cloneDeep(images)
-})
-
-export const game = ({questions = []}) => (
-  {questions: [...questions]}
-)
